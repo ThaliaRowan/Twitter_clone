bae02d00f99e5cfeede8d28c5c7586da2d19f3d1
ThaliaRowan <thalia.rowan@gmail.com>
2021-04-13 13:39:15 -0300

    small change

--- clone/src/TweetBox.js
+++ clone/src/TweetBox.js
@@ -27,7 +27,7 @@ function TweetBox() {
         <div className="tweetBox">
             <form>
                 <div className="tweetbox_Input">
-                <Avatar src="https://i.pinimg.com/originals/ff/a9/d2/ffa9d2dd2cd0f773e9b7ef59671116f0.jpg" />
+                <Avatar src="https://i.pinimg.com/originals/ff/a9/d2/ffa9d2dd2cd0f773e9b7ef59671116f0.jpg"/>
                 <input onChange={(e) => setTweet(e.target.value)} value={tweet} placeholder="What's happening?" type="text" />
             
                 </div>
